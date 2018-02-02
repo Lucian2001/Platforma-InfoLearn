32298442fd8596c07cca548b8883a8128440aee2
LucianGabriel <gabriellucian@rocketmail.com>
2018-02-03 01:28:02 +0200

    update

--- scripturi/angularscript.js
+++
@@ -1,9 +0,0 @@
-var app = angular.module('myApp', ['ngRoute','ng-fi-text','ngSanitize']);
-
-  app.config(function($routeProvider) {
-    $routeProvider .when("/", {
-    templateUrl : "page/test2.html",
- });
- });
-
-
